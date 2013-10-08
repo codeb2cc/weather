735076aaf411ebc06bbf226deb492d4ecafb521b
Codeb Fan <codeb2cc@gmail.com>
2013-10-08 12:25:25 +0800

    移除早晚天气播报. 增加上海地区降水监测

--- monitor/celery.py
+++ monitor/celery.py
@@ -5,7 +5,6 @@ from __future__ import absolute_import, division, print_function, with_statement
 from datetime import timedelta
 
 from celery import Celery
-from celery.schedules import crontab
 
 
 celery = Celery('weather', broker='beanstalk://localhost:11300')
@@ -16,26 +15,6 @@ celery.conf.update(
     CELERY_IGNORE_RESULT=True,
     CELERYD_MAX_TASKS_PER_CHILD=100,
     CELERYBEAT_SCHEDULE={
-        'boardcast-010-am': {
-            'task': 'weather.monitor.periodic.boardcast',
-            'schedule': crontab(hour=8, minute=0, day_of_week=[1, 2, 3, 4, 5]),
-            'args': ('Z9010', '北京地区当前气象状况 - 基本反射率（降水）'),
-        },
-        'boardcast-010-pm': {
-            'task': 'weather.monitor.periodic.boardcast',
-            'schedule': crontab(hour=18, minute=0, day_of_week=[1, 2, 3, 4, 5]),
-            'args': ('Z9010', '北京地区当前气象状况 - 基本反射率（降水）'),
-        },
-        'boardcast-200-am': {
-            'task': 'weather.monitor.periodic.boardcast',
-            'schedule': crontab(hour=7, minute=30, day_of_week=[1, 2, 3, 4, 5]),
-            'args': ('Z9200', '广州地区当前气象状况 - 基本反射率（降水）'),
-        },
-        'boardcast-200-pm': {
-            'task': 'weather.monitor.periodic.boardcast',
-            'schedule': crontab(hour=17, minute=30, day_of_week=[1, 2, 3, 4, 5]),
-            'args': ('Z9200', '广州地区当前气象状况 - 基本反射率（降水）'),
-        },
         'sampling-010': {
             'task': 'weather.monitor.periodic.sampling',
             'schedule': timedelta(minutes=20),
@@ -45,6 +24,11 @@ celery.conf.update(
             'task': 'weather.monitor.periodic.sampling',
             'schedule': timedelta(minutes=20),
             'args': ('Z9200', '广州地区降雨预警 [下雨][下雨]'),
+        },
+        'sampling-210': {
+            'task': 'weather.monitor.periodic.sampling',
+            'schedule': timedelta(minutes=20),
+            'args': ('Z9210', '上海地区降雨预警 [下雨][下雨]'),
         },
     },
     ADMINS=(('Codeb Fan', 'codeb2cc@163.com'), ),
